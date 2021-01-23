63988fab52d6cce3e7d230c6882cb400fb530f4e
Poriya Monfared <poriya.monfared@gmail.com>
2021-01-23 15:08:49 +0300

    init basic #1

---
+++ src/services/recordService.js
@@ -0,0 +1,21 @@
+﻿const { Record } = require("../models/record");
+
+module.exports = {
+    getAllByFilter: async function (startDate, endDate, minCount, maxCount) {
+        const records = await Record.find()
+            .sort({ field: 'asc', _id: -1 });
+
+        let rqList = [];
+        if(records && records.length > 0){
+            for (record of records){
+                let rq = record.toObject();
+                rqList.push(_.pick(rq, [
+                    "key",
+                    "createdAt",
+                    "counts"
+                ]));
+            }
+        }
+        return rqList;
+    },
+};
